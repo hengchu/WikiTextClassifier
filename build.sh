c869d098b9dfd64b34fbdcd6d845b597f1b42bd3
hengchu <hengchu.zhang@gmail.com>
2019-05-09 18:39:05 -0400

    finishing aws lambda deployment

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -o xtrace
 
 rm -rf /tmp/build
 mkdir -p /tmp/build
@@ -9,4 +10,9 @@ python3 -m pip install -r lean-requirements.txt -t /tmp/build
 python3 -m pip install . -t /tmp/build
 
 cd /tmp/build
-zip -r /io/lambda.zip *
+zip -q -r /io/lambda.zip * \
+    -x 'caffe2/*' \
+    -x 'torch/*' \
+    -x 'torch-*.dist-info/*'
+zip -q -r /io/torch.zip torch/* torch-*.dist-info
+zip -q -r /io/caffe2.zip caffe2/*
